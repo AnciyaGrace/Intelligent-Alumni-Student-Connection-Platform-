9cbf895ad08c34bebf0f09cddc2b0315713f194d
AnciyaGrace <anciyagrace@gmail.com>
2024-11-09 07:20:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-<body>
-  <script type="module">
-    import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js'
 
-    // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-analytics.js'
-
-    // Add Firebase products that you want to use
-    import { getAuth } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js'
-    import { getFirestore } from 'https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js'
-  </script>
-</body>
